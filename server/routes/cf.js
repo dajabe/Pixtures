c24e50ae969fc3e9289575bf891da822fcb1132a
David Benton <30560845+dajabe@users.noreply.github.com>
2022-06-30 02:57:55 +1200

    Added second function to experiment with alternative ways of pulling the data

--- server/routes/cf.js
+++ server/routes/cf.js
@@ -20,6 +20,7 @@ router.get('/mens', async (req, res) => {
   try {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
+    // Wait for the page to load properly first
     await page.goto(url, { waitUntil: 'networkidle2' })
 
     // selector = #fe-draws-list-1043332 > table > tbody > tr:nth-child(2) > td.abbr-name-comp.home-team
@@ -35,6 +36,37 @@ router.get('/mens', async (req, res) => {
     })
     await browser.close()
     res.send(fixtures)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+// Alternative method using table tr and td as selectors
+router.get('/menstd', async (req, res) => {
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    // Wait for the page to load properly first
+    await page.goto(url, { waitUntil: 'networkidle2' })
+
+    // selector = #fe-draws-list-1043332 > table > tbody > tr:nth-child(2) > td.abbr-name-comp.home-team
+    const data = await page.evaluate(() => {
+      // Select based on CSS row which has an outerText property that contains game details
+      // Additional selector to try isolate rows in specific draw lists. Cup and League games but returns null for some reason.
+      return Array.from(document.querySelectorAll('.fe-draws-list')).map(
+        (comp) => {
+          Array.from(comp.querySelectorAll('table tr'))
+            .filter((row) => Array.from(row.querySelectorAll('td')).length > 1)
+            .map((row) => {
+              return Array.from(row.querySelectorAll('td'))
+                .filter((col) => col.innerText.length > 0)
+                .map((col) => col.innerText)
+            })
+        }
+      )
+    })
+    await browser.close()
+    res.send(data)
   } catch (err) {
     res.status(500).send(err.message)
   }
