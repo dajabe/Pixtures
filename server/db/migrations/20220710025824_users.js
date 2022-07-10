4a9fc045755add2bf0037593b781018f178313fa
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-10 15:06:52 +1200

    Add users migration

--- server/db/migrations/20220710025824_users.js
+++ server/db/migrations/20220710025824_users.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  knex.schema.createTable('users', (table) => {
+    table.string('auth_id').primary()
+    table.string('name')
+    table.string('email')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  knex.schema.dropTable('users')
+}
