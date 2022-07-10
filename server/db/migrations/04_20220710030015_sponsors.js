2928db26d1a61bf84abf8129daee9a76f77c96d8
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-10 19:10:12 +1200

    Add sponsors migration

--- server/db/migrations/04_20220710030015_sponsors.js
+++ server/db/migrations/04_20220710030015_sponsors.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('sponsors', (t) => {
+    t.uuid('id').primary
+    t.string('name')
+    t.string('logo')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('sponsors')
+}
