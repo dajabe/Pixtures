c9513e2cd24faca1a0c8e5523c2542205190304f
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-10 19:07:45 +1200

    Add team_sponsors migration

--- server/db/migrations/06_20220710030027_team_sponsors.js
+++ server/db/migrations/06_20220710030027_team_sponsors.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('team_sponsors', (t) => {
+    t.integer('team_id')
+    t.integer('sponsor_id')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('team_sponsors')
+}
