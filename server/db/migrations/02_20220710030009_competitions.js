f68dfb07600fc958c986ae0dd180af3d58da10fb
David Benton <30560845+dajabe@users.noreply.github.com>
2022-07-10 19:12:59 +1200

    Add competitions migration

--- server/db/migrations/02_20220710030009_competitions.js
+++ server/db/migrations/02_20220710030009_competitions.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('competitions', (t) => {
+    t.uuid('id').primary()
+    t.string('name')
+    t.string('type')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('competitions')
+}
